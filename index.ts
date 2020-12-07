0f20a632e332a5231738c5e2beeca6dd6dfbecbe
aasereb6 <aasereb6@mts.ru>
2020-12-07 21:23:19 +0300

    add: пробую добавить данные в БД

--- index.ts
+++ index.ts
@@ -3,40 +3,54 @@ import { Collection, MongoClient } from 'mongodb';
 import { Person } from "./models/person";
 import {MONGO_OPTIONS} from './mongo/config/options'
 import { MONGO_URL } from "./mongo/config/url";
+import { readFileSync } from "fs";
 
 const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 const port = 3000;
 
-MongoClient.connect(MONGO_URL, MONGO_OPTIONS, function(err, db) {
-	const dbo = db.db("ntv-db");
-	// ... dbo.collection(...).find(...)
-	console.log('подключились к бд')
-});
+const peopleFile = readFileSync('./people2020.json', 'utf8');
+const ordersFile = readFileSync('./orders2020.json', 'utf8');
+
+const peopleArr = JSON.parse(peopleFile);
+const ordersArr = JSON.parse(ordersFile);
 
 app.use(express.static('front/natvorim-crm'));
 app.use(express.json());
+app.set('trust proxy', '84.201.179.234')
 
-// let dbClient: MongoClient;
+MongoClient.connect(MONGO_URL, MONGO_OPTIONS, function (err, client) {
+	if (err) return console.error(err);
 
-// mongoClient.connect((err, client) => {
-// 	if (err) return console.error(err);
+	const db = client.db("ntv-db");
+	const peopleCollection = db.collection("people")
+	const ordersCollection = db.collection("orders");
+
+	peopleCollection.insertMany(peopleArr, function (err, results) {     
+		if (err) {
+			console.log('ошибка в добавлении людей')
+			return
+		}
+        console.log('добавили людей');
+	});
 	
-// 	const db = client.db("testdb");
-// 	const peopleCollection = db.collection("people")
-// 	const ordersCollection = db.collection("orders");
-	
-// 	dbClient = client;
+	ordersCollection.insertMany(ordersArr, function (err, results) {     
+		if (err) {
+			console.log('ошибка в добавлении заказов')
+			return
+		}
+        console.log('добавили заказы');
+    });
 
-//     app.locals.people = peopleCollection;
-// 	app.locals.orders = ordersCollection;
 
-    app.listen(port, function(){
-		console.log(`Сервер ожидает подключения на порту ${port}`);
+    app.locals.people = peopleCollection;
+	app.locals.orders = ordersCollection;
+
+	app.listen(port, function(){
+		console.log(`Подключились к БД, сервер ожидает на порту ${port}`);
 	});
 
-
-// });
+})
 
 
 
@@ -71,9 +85,3 @@ app.post('/orders/by/personid', (req, res) => {
 	})
 
 })
-
-// // прослушиваем прерывание работы программы (ctrl-c)
-// process.on("SIGINT", () => {
-//     dbClient.close();
-//     process.exit();
-// });
