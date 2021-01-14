cc5734f679528d33f3950a5ee9d46cf8593a473d
aasereb6 <aasereb6@mts.ru>
2021-01-14 17:12:04 +0300

    add: поиск людей по следующим критериям: имя, любой контакт, комментарий к заказу

--- router/customers/customers-router.ts
+++ router/customers/customers-router.ts
@@ -4,13 +4,15 @@ import { getCustomers } from "./handlers/get";
 import { patchCustomer } from "./handlers/patch";
 import { postCustomer } from "./handlers/post";
 import { removeCustomer } from "./handlers/remove";
+import { customerSearch } from "./handlers/search";
 
 const handlers: RequestHandler[] = [
 	getCustomers,
 	customerAutocomplete,
 	postCustomer,
 	patchCustomer,
-	removeCustomer
+	removeCustomer,
+	customerSearch
 ]
 
 export const customersRouter = Router().use('/people', ...handlers);
---
+++ router/customers/handlers/search.ts
@@ -0,0 +1,62 @@
+import { Router } from 'express';
+import { Collection } from 'mongodb';
+import { Order } from '../../../models/order';
+import { Person } from '../../../models/person';
+
+export const customerSearch = Router();
+	
+customerSearch.post('/search', (req, res) => {
+
+	const peopleCollection: Collection<Person> = req.app.locals.people;
+	const ordersCollection: Collection<Order> = req.app.locals.orders;
+
+	const { query }: { query: string } = req.body;
+
+	const phoneQuery = query.split('').map(v => {
+		if (v.includes('(') || v.includes(')')) {
+			return "\\" + v;
+		}
+		return v;
+	}).join('')
+
+	const criteria = {
+		$or: [
+			{
+				name: { $regex: query, $options: 'i' },
+			},
+			{
+				'contacts.inst': { $regex: query, $options: 'i' },
+			},
+			{
+				'contacts.email': { $regex: query, $options: 'ig' },
+			},
+			{
+				'contacts.phone': { $regex: phoneQuery, $options: 'i' },
+			},
+		]
+	}
+
+	peopleCollection.find(criteria, {limit: 50}).toArray((err, customers: Person[]) => {
+		if (err) return console.log(err);
+
+		if (customers.length) {
+			res.send(customers);
+		} else {
+			ordersCollection.find({ comment: { $regex: query, $options: 'i' } }).toArray((err, orders: Order[]) => {
+				if (err) return console.log(err);
+
+				const personIds = orders.map(o => o.personId)
+
+				peopleCollection.find({ id: { $in: personIds } }).toArray((err, queryCustomers: Person[]) => {
+					if (err) return console.log(err);
+
+					res.send(queryCustomers)
+				})
+
+			})
+		}
+		
+		
+
+	});
+});
